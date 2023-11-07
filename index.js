1c076e6f969c09ce8880f57d1a54259d6b3594ef
Marisol Park <marisolpark@10-18-227-13.dynapool.wireless.nyu.edu>
2023-11-07 09:37:42 -0500

    hello

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const app = express()
-const port = 4000
+const app = express();
+const firebase = require("firebase/app");
+//port
+const port = 4000;
+//My UNIQUE configuration information for firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBPvoDlu_DQ7J8PoWiqdKdoSwumnFoyuIQ",
+    authDomain: "dynamic-web-exercise-fiv-d5059.firebaseapp.com",
+    projectId: "dynamic-web-exercise-fiv-d5059",
+    storageBucket: "dynamic-web-exercise-fiv-d5059.appspot.com",
+    messagingSenderId: "71481793142",
+    appId: "1:71481793142:web:ba87482d994bd59cc59d57"
+  };
+
+firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require("./routes/index");
 // const singlePostRoute = require("./routes/singlePost");
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
+const { error } = require("console");
 const express = require("express");
 const router = express.Router();
+const firestore = require("firebase/firestore");
+const db = firestore.getFirestore();
 
 router.get("/", (req, res) => {
-    res.send("All Posts");
+    const postsQuery = firestore.getDocs(firestore.collection(db, "posts"));
+    const postsArray = [];
+
+    postsQuery
+        .then((response) => {
+            response.forEach((post) => {
+                //console.log(post.data());
+                postsArray.push({id: post.id, ...post.data() });
+            });
+            res.send(postsArray);
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.send(error);
+        });
 });
 
 module.exports = router;
